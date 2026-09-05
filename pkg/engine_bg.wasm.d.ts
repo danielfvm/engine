e3f00a0d8322265719da930e4f9008ea4db3d208
DeanCode <ds@xxxmail.eu>
2026-09-05 20:02:09 +0200

    Added code

---
+++ pkg/engine_bg.wasm.d.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const run_web: () => void;
+export const __wasm_bindgen_func_elem_10293: (a: number, b: number, c: number, d: number) => void;
+export const __wasm_bindgen_func_elem_12105: (a: number, b: number, c: number, d: number) => void;
+export const __wasm_bindgen_func_elem_10289: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_10289_10: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_10289_11: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_10289_5: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_10289_6: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_10289_7: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_10289_8: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_10289_9: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_5442: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_10301: (a: number, b: number) => void;
+export const __wbindgen_export: (a: number, b: number) => number;
+export const __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export3: (a: number) => void;
+export const __wbindgen_export4: (a: number, b: number, c: number) => void;
+export const __wbindgen_export5: (a: number, b: number) => void;
+export const __wbindgen_add_to_stack_pointer: (a: number) => number;
+export const __wbindgen_start: () => void;
